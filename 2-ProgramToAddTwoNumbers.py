68b47d22a65f27b914643f7db81fc25b1ab60b6b
MehmetKaymakçı <61612352+Darkboy00@users.noreply.github.com>
2022-03-21 11:34:09 +0300

    Create 2-ProgramToAddTwoNumbers.py

---
+++ 2-ProgramToAddTwoNumbers.py
@@ -0,0 +1,6 @@
+number1 = input("Please Enter the First Number: ")
+number2 = input("Please Enter the Second Number: ")
+
+# Using arithmetic + Operator to add two numbers
+sum = float(number1) + float(number2)
+print('The sum of {0} and {1} is'.format(sum, number1, number2))
