b8910e0b8eda24541e9fafe7101f5caea99d91ab
MehmetKaymakçı <61612352+Darkboy00@users.noreply.github.com>
2022-06-29 16:15:54 +0300

    Python

---
+++ Python.py
@@ -0,0 +1,107 @@
+# Numbers and Strings 
+
+"""
+a = 9
+b = 10
+print(a * b)
+print("Hello World")
+print(91)
+"""
+
+# Strings
+
+"""
+print("a" + "b")
+print("a"*3)
+"""
+
+# String Methods - len() 
+
+"""
+h = "Hello World"
+print(len(h))
+print(len("Hello World"))
+"""
+
+# String Methods - upper() & lower()
+
+"""
+h = "hello"
+print(h.upper())
+print(h.lower())
+print(h.islower())
+
+H = h.upper()
+print(H.isupper())
+print(H.islower())
+"""
+
+# String Methods - replace()
+
+"""
+h = "hello"
+print(h.replace("e","a"))
+"""
+
+# String Methods - strip()
+
+"""
+h = " hello "
+print(h.strip())
+h = "-hello-"
+print(h.strip("-"))
+"""
+
+# Methods 
+
+"""
+h = "hello world"
+print(dir(h))
+print(h.capitalize())
+print(h.title())
+"""
+
+# Substrings
+
+"""
+h = "hello world"
+print(h[0])
+print(h[0:6])
+"""
+
+# Variables
+
+"""
+a = 9
+b = "hello"
+c = a * 2
+
+print(a/c)
+print(a*c)
+print(a*3)
+
+print(type(2))
+print(type(100.2))
+print(type(1+2j))
+"""
+
+# Type Conversions
+
+"""
+a = int(input())
+b = int(input())
+
+print(type(a))
+print(a + b)
+print(int(11.0))
+print(float(11))
+print(type(str(12)))
+"""
+
+# print()
+
+"""
+print("hello world")
+print("hello","world")
+print("hello","world", sep = "_")
+"""
